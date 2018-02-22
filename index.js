0dac31a8bd16b4b695d1c5e80abeddf96059be8f
Anil Kumar krishanshetty <anilbms75@gmail.com>
2018-02-22 09:55:21 +0100

    update sample demo.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function layer(context, layer) {
-    return "Sample " + layer.name + "."
+    var object = {
+        "layerName": layer.name,
+        "projectName": context.project.name
+    };
+
+    var JSONString = JSON.stringify(object, null, 2);
+
+    return {
+        code: JSONString,
+        language: "json"
+    };
 };
